557568e6e1443bbf11a3e2f5ca5144ad9b770bca
Huy-Tuan <sieuka1990@gmail.com>
2026-05-03 21:45:52 +0700

    submit

--- src/main/java/com/btvn/ss16/repository/OrderRepository.java
+++ src/main/java/com/btvn/ss16/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.btvn.ss16.repository;
 
 import com.btvn.ss16.model.Order;
+import com.btvn.ss16.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByOrderByOrderedAtDesc();
+
+    List<Order> findByUserOrderByOrderedAtDesc(User user);
 }
