faf2933b4c0bfa563a28617c13c6ba3b8361fb15
Huy-Tuan <sieuka1990@gmail.com>
2026-05-03 15:12:22 +0700

    task2

--- src/main/java/com/btvn/ss16/repository/OrderDetailRepository.java
+++ src/main/java/com/btvn/ss16/repository/OrderDetailRepository.java
@@ -1,4 +1,7 @@
 package com.btvn.ss16.repository;
 
-public interface OrderDetailRepository {
+import com.btvn.ss16.model.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
 }
--- src/main/java/com/btvn/ss16/specification/ProductSpecification.java
+++ src/main/java/com/btvn/ss16/specification/ProductSpecification.java
@@ -1,5 +1,47 @@
 package com.btvn.ss16.specification;
 
+import com.btvn.ss16.model.Product;
+import org.springframework.data.jpa.domain.Specification;
+
 // dynamic search
 public class ProductSpecification {
+
+    public static Specification<Product> hasName(String name) {
+        return (root, query, criteriaBuilder) -> {
+            if (name == null || name.isEmpty()){
+                return null;
+            }
+            return criteriaBuilder.like(
+                    criteriaBuilder.lower(root.get("name")),
+                    "%" + name.toLowerCase() + "%"
+            );
+        };
+    }
+
+    public static Specification<Product> hasMinPrice (Double minPrice) {
+        return (root, query, criteriaBuilder) -> {
+            if (minPrice == null) {
+                return null;
+            }
+            return criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+        };
+    }
+
+    public static Specification<Product> hasMaxPrice (Double maxPrice) {
+        return (root, query, criteriaBuilder) -> {
+            if (maxPrice == null) {
+                return null;
+            }
+            return criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+        };
+    }
+
+    public static Specification<Product> hasCategory (Long categoryId) {
+        return (root, query, criteriaBuilder) -> {
+            if (categoryId == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("category").get("id"), categoryId);
+        };
+    }
 }
